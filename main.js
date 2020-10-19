f71c75a50a3276694af4e2844f4fb2f1df91d673
Kasia Laniecka <kasia.laniecka@gmail.com>
2020-10-19 23:38:20 +0200

    polishing mobile

---
+++ main.js
@@ -0,0 +1,19 @@
+// APPEAR EFFECT
+let appearEffectSections = [".background-text", ".background-img", ".skills-inner", ".work-inner", ".contact-inner"];
+
+function scrollAppear(className) {
+    let section = document.querySelector(className)
+    let sectionPosition = section.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight / 1.3;
+    if (sectionPosition < screenPosition) {
+        section.classList.add('appear-effect');
+    } else {
+        section.classList.remove('appear-effect');
+    }
+}
+
+window.addEventListener('scroll', () => {
+    appearEffectSections.forEach(section => {
+        scrollAppear(section);
+    })
+});
