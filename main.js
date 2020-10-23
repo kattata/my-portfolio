bd48bf9b2ec2a0cfdb7646adc0c8449039ef294a
Kasia Laniecka <kasia.laniecka@gmail.com>
2020-10-24 00:57:41 +0200

    hoverss

--- main.js
+++ main.js
@@ -30,3 +30,26 @@ function changeBgColor() {
 
 window.addEventListener('scroll', changeBgColor);
 
+// CONTACT HOVER
+let emailContainer = document.querySelector('.email-container');
+let email = document.querySelector('.email');
+let linkedinContainer = document.querySelector('.linkedin-container');
+let linkedin = document.querySelector('.linkedin');
+let githubContainer = document.querySelector('.github-container');
+let github = document.querySelector('.github');
+
+function changeContactHome(container, image, name) {
+    container.addEventListener('mouseenter', function () {
+        image.src = `media/white-${name}.svg`;
+    })
+    container.addEventListener('mouseleave', function () {
+        image.src = `media/${name}.svg`;
+    })
+}
+
+changeContactHome(emailContainer, email, 'envelope');
+changeContactHome(linkedinContainer, linkedin, 'linkedin');
+changeContactHome(githubContainer, github, 'github');
+
+
+
